474459fb1f10ae7a556bda0bebdb225600ef3c3c
EldarHasanli-maker <76447592+EldarHasanli-maker@users.noreply.github.com>
2021-08-16 12:44:35 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -1,3 +1,6 @@
+difference=0;
+leftwristX=0;
+rightwristX=0;
 function setup(){
     video=createCapture(VIDEO);
     video.size(550,500);
@@ -8,12 +11,21 @@ function setup(){
 }
 function draw(){
     background("#10a38d");
+    fill("#b80d18");
+    text("Eldar",50,300);
+    textSize(difference);
+    document.getElementById("font_size").innerHTML="Font size of the text will be="+difference+"px";
 }
+
 function modelLoaded(){
     console.log("poseNet is initialized");
 }
 function gotPoses(results){
     if(results.length > 0){
         console.log(results);
+        leftwristX=results[0].pose.leftWrist.x;
+        rightwristX=results[0].pose.rightWrist.x;
+        difference=floor(leftwristX-rightwristX);
+        console.log("leftwristX= "+leftwristX+"rightwristX= "+rightwristX+"difference="+difference);
     }
 }
